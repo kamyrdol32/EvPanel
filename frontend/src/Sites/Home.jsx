5a9316c5b481c4317a589921c43b09d5522240f9
kamyrdol32 <kamyrdol32@gmail.com>
2023-04-03 22:34:59 +0200

    Update 1.0.8 Website monitor

--- frontend/src/Sites/Home.jsx
+++ frontend/src/Sites/Home.jsx
@@ -46,33 +46,35 @@ export default function Home() {
                     <div className="flow-root">
                         <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
                             {websites && websites.map((item, index) => (
-                                <li className="py-3 sm:py-4">
-                                    <div className="flex items-center space-x-4">
-                                        {item.status === "Online" ? (
-                                            <HiWifi className="h-5 w-5 text-green-400" aria-hidden="true"/>
-                                        ) : (
-                                            <HiWifi className="h-5 w-5 text-red-400" aria-hidden="true"/>
-                                        )}
-                                        <div className="flex-1 min-w-0">
-                                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                {item.name}
-                                            </p>
-                                            <a href={item.url} className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                {item.url}
-                                            </a>
-                                        </div>
-                                        {item.status === "Online" ? (
-                                            <div className=" inline-flex items-center text-base font-semibold text-green-600">
-                                                {item.status}
+                                <div key={item.name}>
+                                    <li className="py-3 sm:py-4">
+                                        <div className="flex items-center space-x-4">
+                                            {item.status === "Online" ? (
+                                                <HiWifi className="h-5 w-5 text-green-400" aria-hidden="true"/>
+                                            ) : (
+                                                <HiWifi className="h-5 w-5 text-red-400" aria-hidden="true"/>
+                                            )}
+                                            <div className="flex-1 min-w-0">
+                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                    {item.name}
+                                                </p>
+                                                <a href={item.url} className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                    {item.url}
+                                                </a>
                                             </div>
-                                        ) : (
-                                            <div className=" inline-flex items-center text-base font-semibold text-red-600">
-                                                {item.status}
-                                            </div>
-                                        )}
+                                            {item.status === "Online" ? (
+                                                <div className=" inline-flex items-center text-base font-semibold text-green-600">
+                                                    {item.status}
+                                                </div>
+                                            ) : (
+                                                <div className=" inline-flex items-center text-base font-semibold text-red-600">
+                                                    {item.status}
+                                                </div>
+                                            )}
 
-                                    </div>
-                                </li>
+                                        </div>
+                                    </li>
+                                </div>
                             ))}
                         </ul>
                     </div>
