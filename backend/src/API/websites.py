58cd00ccdd3d563b36473d37b16f5708fd37a6d1
kamyrdol32 <kamyrdol32@gmail.com>
2023-04-04 13:09:45 +0200

    Update 1.0.8 Website monitor

--- backend/src/API/websites.py
+++ backend/src/API/websites.py
@@ -2,6 +2,7 @@ import requests
 import time
 
 from flask import jsonify, request
+from flask_cors import cross_origin
 from flask_openapi3 import APIBlueprint
 
 from ..app import db
@@ -11,11 +12,32 @@ websites_blueprint = APIBlueprint("website", __name__, url_prefix="/website")
 
 
 @websites_blueprint.get("/get")
+@cross_origin()
 def get_websites():
-    check_websites()
-    websites = Websites.query.all()
     Data = []
+    websites = Websites.query.all()
+
     for website in websites:
+        try:
+            if website.endpoint:
+                response = requests.get(website.endpoint)
+                if response.status_code == 200:
+                    website.status = "Online"
+                    print("Online " + str(website.name))
+                else:
+                    website.status = "Offline"
+                    print("Offline " + str(website.name))
+            else:
+                website.status = "Unknown"
+
+        except Exception as error:
+            website.status = "ERROR"
+            print(error)
+            return jsonify({"error": str(error)}), 400
+
+        db.session.add(website)
+        db.session.commit()
+
         Data.append(
             {
                 "id": website.id,
@@ -24,29 +46,5 @@ def get_websites():
                 "status": website.status,
             }
         )
+
     return jsonify(Data), 200
-
-
-def check_websites():
-    websites = Websites.query.all()
-    for website in websites:
-        try:
-            if website.endpoint != "":
-                response = requests.get(website.endpoint)
-                if response.status_code == 200:
-                    website.status = "Online"
-                    print("Online " + str(website.name))
-                else:
-                    website.status = "Offline"
-                    print("Offline " + str(website.name))
-
-                db.session.add(website)
-                db.session.commit()
-
-        except Exception as error:
-            website.status = "Offline"
-            db.session.add(website)
-            db.session.commit()
-
-            print(error)
-            return jsonify({"error": str(error)}), 400
