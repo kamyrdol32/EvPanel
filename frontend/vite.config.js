11390ed3a6f635ea62c4d0e13613417a4a97bace
kamyrdol32 <kamyrdol32@gmail.com>
2023-04-06 00:47:00 +0200

    Update 1.1.2 Websites Monitoring

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        origin: "https://evpanel.kamilzeglen.pl",
         https: true,
         watch: {
             usePolling: true,
