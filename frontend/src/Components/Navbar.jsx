083c9fe7cf581dffe5e2c8903882755d858c41e1
kamyrdol32 <kamyrdol32@gmail.com>
2023-04-15 00:25:37 +0200

    Update 1.2.1 PyTest + Fixes

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar() {
     useQuery(['Authorization'], fetchAuthorization)
 
     return (
-        <Disclosure as="nav" className="bg-primary dark:bg-dark_primary border-accent">
+        <Disclosure as="nav" className="bg-primary dark:bg-dark_primary border-accent text-white">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
